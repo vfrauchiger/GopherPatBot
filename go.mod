a939eaa9609edbb8c66e394951958b5bc7bec71d
Vinz Frauchiger <frvi2@ypsomed.com>
2021-12-09 16:29:30 +0100

    first experiments

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vfrauchiger/GopherPatBot
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+var consKey string = "weOTMHzHLtGFMGyxLorxfCkDyNYGPpUE "
+var consSecKey string = "1GDjAGpvc7nuEeKB"
+
+type AuthResponse struct {
+	RefreshTokenExpiresIn string   `json:"refresh_token_expires_in"`
+	APIProductList        string   `json:"api_product_list"`
+	APIProductListJSON    []string `json:"api_product_list_json"`
+	OrganizationName      string   `json:"organization_name"`
+	DeveloperEmail        string   `json:"developer.email"`
+	TokenType             string   `json:"token_type"`
+	IssuedAt              string   `json:"issued_at"`
+	ClientID              string   `json:"client_id"`
+	AccessToken           string   `json:"access_token"`
+	ApplicationName       string   `json:"application_name"`
+	Scope                 string   `json:"scope"`
+	ExpiresIn             string   `json:"expires_in"`
+	RefreshCount          string   `json:"refresh_count"`
+	Status                string   `json:"status"`
+}
+
+type WorldPatentData struct {
+	XMLName         xml.Name `xml:"world-patent-data"`
+	Text            string   `xml:",chardata"`
+	Xmlns           string   `xml:"xmlns,attr"`
+	Ops             string   `xml:"ops,attr"`
+	Xlink           string   `xml:"xlink,attr"`
+	DocumentInquiry struct {
+		Text                 string `xml:",chardata"`
+		PublicationReference struct {
+			Text       string `xml:",chardata"`
+			DocumentID struct {
+				Text           string `xml:",chardata"`
+				DocumentIDType string `xml:"document-id-type,attr"`
+				Country        string `xml:"country"`
+				DocNumber      string `xml:"doc-number"`
+				Kind           string `xml:"kind"`
+			} `xml:"document-id"`
+		} `xml:"publication-reference"`
+		InquiryResult struct {
+			Text                 string `xml:",chardata"`
+			PublicationReference struct {
+				Text       string `xml:",chardata"`
+				DocumentID struct {
+					Text           string `xml:",chardata"`
+					DocumentIDType string `xml:"document-id-type,attr"`
+					Country        string `xml:"country"`
+					DocNumber      string `xml:"doc-number"`
+					Kind           string `xml:"kind"`
+				} `xml:"document-id"`
+			} `xml:"publication-reference"`
+			DocumentInstance []struct {
+				Text                  string `xml:",chardata"`
+				System                string `xml:"system,attr"`
+				NumberOfPages         string `xml:"number-of-pages,attr"`
+				Desc                  string `xml:"desc,attr"`
+				Link                  string `xml:"link,attr"`
+				DocumentFormatOptions struct {
+					Text           string   `xml:",chardata"`
+					DocumentFormat []string `xml:"document-format"`
+				} `xml:"document-format-options"`
+				DocumentSection []struct {
+					Text      string `xml:",chardata"`
+					Name      string `xml:"name,attr"`
+					StartPage string `xml:"start-page,attr"`
+				} `xml:"document-section"`
+			} `xml:"document-instance"`
+		} `xml:"inquiry-result"`
+	} `xml:"document-inquiry"`
+}
+
+func authenticatePB() string {
+	authURL := "https://ops.epo.org/3.2/auth/accesstoken"
+	encKey := consKey + ":" + consSecKey
+	encKey = base64.StdEncoding.EncodeToString([]byte(encKey))
+	//Body of R
+	reqBody := strings.NewReader("grant_type=client_credentials")
+
+	req, err := http.NewRequest(
+		"POST",
+		authURL,
+		reqBody,
+	)
+	if err != nil {
+		fmt.Println("Request-Error: ", err)
+	}
+	// Headers
+	req.Header.Add("Authorization", string(encKey))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("Response Error: ", err)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Read Error: ", err)
+	}
+	resp.Body.Close()
+
+	var jsonData AuthResponse
+
+	json.Unmarshal(data, &jsonData)
+
+	fmt.Println("Status: ", resp.StatusCode)
+	fmt.Println(jsonData.AccessToken, jsonData.DeveloperEmail)
+
+	return jsonData.AccessToken
+}
+
+func main() {
+	publno := "ep.1000000.B1"
+	pageURL := "https://ops.epo.org/rest-services/published-data/publication/docdb/" + publno + "/images"
+
+	token := authenticatePB()
+
+	//reqBody := strings.NewReader(publno)
+
+	req, err := http.NewRequest(
+		"GET",
+		pageURL,
+		nil,
+	)
+	if err != nil {
+		fmt.Println("Page Number Req error: ", err)
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	fmt.Println(req.Header)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Response Error: ", err)
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Read Error: ", err)
+	}
+	resp.Body.Close()
+
+	var xmlResp WorldPatentData
+
+	xml.Unmarshal(data, &xmlResp)
+
+	fmt.Println(xmlResp.DocumentInquiry.InquiryResult.DocumentInstance[0].NumberOfPages)
+}
